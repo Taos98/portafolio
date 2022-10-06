c886f908aa02ebd411d7eea9f62dc5cc24a2e1ff
tmontecinos <tomasmmontecinos@gmail.com>
2022-10-05 22:50:45 -0300

    feat: links to github and linkedin, personalized card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'portafolio';
-  content: any = [
-    {
-      header: 'Hola Mundo',
-      text: '123'
-    },
-    {
-      header: 'Hola Mundo 2',
-      text: '1233'
-    },
-    {
-      header: 'Hola Mundo 3',
-      text: '12334'
-    },
-    {
-      header: 'Hola Mundo 4',
-      text: '123345'
-    },
-  ]
+  content: any = [];
+  education: any = {};
+  loading: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.setData();
+    }
+      , 3000);
+  }
+
+  setData() {
+    this.loading = true;
+    this.content = [
+      {
+        header: 'Hola Mundo',
+        text: '123',
+        
+      },
+      {
+        header: 'Hola Mundo 2',
+        text: '1233'
+      },
+      {
+        header: 'Hola Mundo 3',
+        text: '12334'
+      },
+      {
+        header: 'Hola Mundo 4',
+        text: '123345'
+      },
+    ];
+    this.education = {
+      header: 'Universidad del Bío-Bío',
+      text: 'Ingeniería de Ejecución en Computación e Informática',
+      subHeader: '[2016 - 2022]',
+      info: 'Egresado, actualmente trabajando en una empresa de desarrollo de software de un CMS para la gestión de contenidos de múltiples empresas.',
+      image: 'https://ubiobio.cl/w/img/Escudo-UBB.svg',
+      ImageStyle: {
+        'width': '60%',
+        'height': '60%',
+        'margin-top': '2em',
+      }
+    };
+    this.loading = false;
   }
 }
